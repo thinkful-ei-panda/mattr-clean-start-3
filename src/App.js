583e76569609ee9debc8d57314304e5c58052c5f
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-27 01:02:51 -0700

    this commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Box from './composition/Box';
+import Card from './composition/Card'
+import './App.css';
+import STORE from './composition/STORE'
+// import List from './composition/List'
+
+
 
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
+      <Box className='left' flexBasis={1}>
+        <h2>{STORE.lists[0].header}</h2>
+      <Card>
+      <h3></h3>
+      <p>The description of the card</p>
+      <button className='delete'>Delete</button>
+      </Card>
+      </Box>
+      <Box className='right' name='Untitled Card'>
+        This is the content children from the RIGHT box.
+      
+      </Box>
+      
+    
     </main>
   );
 }
---
+++ src/composition/List.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function List(props) {
+    return (
+        <section>
+      {props.children}
+            
+        </section>
+    )
+}
